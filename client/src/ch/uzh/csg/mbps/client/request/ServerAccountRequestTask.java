83ebc387165349cfcc6f8806109994629228c732
malib <mehmetali.bekooglu@uzh.ch>
2014-08-25 16:06:45 +0200

    updated request

--- client/src/ch/uzh/csg/mbps/client/request/ServerAccountRequestTask.java
+++ client/src/ch/uzh/csg/mbps/client/request/ServerAccountRequestTask.java
@@ -1,38 +1,24 @@
 package ch.uzh.csg.mbps.client.request;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.util.UriComponentsBuilder;
-
+import net.minidev.json.JSONObject;
 import ch.uzh.csg.mbps.client.IAsyncTaskCompleteListener;
-import ch.uzh.csg.mbps.client.servercomm.CookieHandler;
-import ch.uzh.csg.mbps.client.servercomm.CustomRestTemplate;
 import ch.uzh.csg.mbps.client.util.Constants;
-import ch.uzh.csg.mbps.responseobject.CustomResponseObject;
+import ch.uzh.csg.mbps.responseobject.ServerAccountObject;
+import ch.uzh.csg.mbps.responseobject.ServerAccountTransferObject;
 
 /**
  * This class sends a request to retrieve the server accounts.
  */
-public class ServerAccountRequestTask extends RequestTask {
-	private static final String URL_PAGE_STRING = "urlPage";
+public class ServerAccountRequestTask extends RequestTask<ServerAccountObject, ServerAccountTransferObject> {
 	
-	public ServerAccountRequestTask(IAsyncTaskCompleteListener<CustomResponseObject> cro, int urlPage) {
-		this.callback = cro;
-		setParamteres(urlPage);
+	public ServerAccountRequestTask(IAsyncTaskCompleteListener<ServerAccountTransferObject> cro, ServerAccountObject input, ServerAccountTransferObject output) {
+		super(input, output, Constants.BASE_URI_SSL + "/serveraccounts/accounts", cro);
 	}
-	
-	private void setParamteres(int urlPage) {
-		this.url = UriComponentsBuilder.fromUriString(Constants.BASE_URI_SSL)
-			    .path("/serveraccounts/accounts")
-			    .queryParam(URL_PAGE_STRING, urlPage)
-			    .build()
-			    .toUriString();
-	}
-	
+
 	@Override
-	protected CustomResponseObject responseService(CustomRestTemplate restTemplate) {
-		@SuppressWarnings("rawtypes")
-		HttpEntity requestEntity = CookieHandler.getAuthHeader();
-		return restTemplate.exchange(url, HttpMethod.GET, requestEntity);
+	protected ServerAccountTransferObject responseService(ServerAccountObject sao) throws Exception {
+		JSONObject jsonObject = new JSONObject();
+		sao.encode(jsonObject);
+		return execPost(jsonObject);
 	}
 }
